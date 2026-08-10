d26e839ee4b15e3aa2d85a43e016778d9b2caec0
asdf13110791253 <60729322+asdf13110791253@users.noreply.github.com>
2026-08-10 18:57:27 +0800

    Update SplashActivity.kt

--- app/src/main/java/com/babyjie/SplashActivity.kt
+++ app/src/main/java/com/babyjie/SplashActivity.kt
@@ -3,11 +3,23 @@ package com.babyjie
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 全屏隐藏状态栏和导航栏
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        )
+
         setContentView(R.layout.activity_splash)
 
         val videoView = findViewById<FullScreenVideoView>(R.id.fullScreenVideoView)
