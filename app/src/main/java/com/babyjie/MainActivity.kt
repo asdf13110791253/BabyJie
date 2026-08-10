03b2a876b4665b78d726a5d97d784fa4725ae322
asdf13110791253 <60729322+asdf13110791253@users.noreply.github.com>
2026-08-10 16:14:02 +0800

    Create MainActivity.kt

---
+++ app/src/main/java/com/babyjie/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.babyjie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.babyjie.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
