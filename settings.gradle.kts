3597b68925b7fedb108908fdf8bed005d1c649c9
asdf13110791253 <60729322+asdf13110791253@users.noreply.github.com>
2026-08-14 02:06:18 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,3 @@
-// File: settings.gradle.kts
 pluginManagement {
     repositories {
         google()
@@ -12,7 +11,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 
