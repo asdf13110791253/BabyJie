9c186c294f66c565837b12acc296c82b719ec2b9
asdf13110791253 <60729322+asdf13110791253@users.noreply.github.com>
2026-08-14 00:17:33 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
-// File: build.gradle.kts (Project-level)
-// 项目级构建文件
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("kotlin-kapt") version "1.9.20" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
 }
